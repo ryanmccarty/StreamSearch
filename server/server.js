3108161020a769123ce7e1e4fd89a1f669814dae
jmohon1986 <jmohon1986@gmail.com>
2019-02-19 13:23:07 -0600

    make the client work

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('Angular-Front-End'));
+app.use(express.static('client'));
 app.use(express.static('node_modules'));
 
 
